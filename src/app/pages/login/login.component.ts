8f89fc6929076091262ebeac0c1a14eaa4f2c8dd
Gaurav Seta <36725888+gauravseta03@users.noreply.github.com>
2018-03-28 15:50:22 +0530

    Adding login component and making project structure

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+
+
+@Component({
+    selector : 'app-login',
+    templateUrl: './login.component.html',
+    styles: []
+})
+export class LoginComponent implements OnInit {
+
+    ngOnInit() {
+        
+    }
+}
