924ca0d9f74b9128404793c533cd92d805079b39
Gaurav Seta <36725888+gauravseta03@users.noreply.github.com>
2018-04-03 17:53:11 +0530

    adding auto complete

--- src/app/components/page-layout/page-layout.component.ts
+++ src/app/components/page-layout/page-layout.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UtilService } from '../../services/util.service';
 import { CommonService } from '../../services/common.service';
+import {MemberService} from '../../services/member.service';
 import { AppConstants } from '../../app.constants';
+import {FormControl} from '@angular/forms';
+import {startWith} from 'rxjs/operators/startWith';
+import {map} from 'rxjs/operators/map';
+import {Observable} from 'rxjs/Observable';
 /**
  * Main page layout, display the page side nav and top nav
  */
@@ -14,12 +19,31 @@ import { AppConstants } from '../../app.constants';
 })
 export class PageLayoutComponent implements OnInit {
    private memberName: string = '';
-    constructor(private util: UtilService, private router: Router, private commonService: CommonService){}
+   myControl: FormControl = new FormControl();
+   private membersNameList: any;
+   private filteredOptions: Observable<string[]>;
+    constructor(private util: UtilService, private router: Router, private commonService: CommonService, private memberService: MemberService){}
     ngOnInit() {
       let resolveObj: string= this.util.getFromStorage('SEARCH_TEXT') || '';
      
       this.memberName = resolveObj.substring(1,resolveObj.length-1);
       this.commonService.notifyOther({memberName: this.memberName});
+      this.memberService.getAllMembers().subscribe((res) => {
+        this.membersNameList = res.members.map((d) => {
+          return d.name;
+        });
+      });
+
+      this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(val => this.filter(val))
+      );
+    }
+
+    filter(val: string): string[] {
+      return this.membersNameList.filter(option =>
+        option.indexOf(val) === 0);
     }
 
     /**
